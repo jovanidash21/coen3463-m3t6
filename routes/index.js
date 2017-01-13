9a9a752456ee1e7f5e5e4308bf67a80a5a8cf8b3
jovanidash21 <jwardash21@gmail.com>
2017-01-14 01:41:20 +0800

    Update Nav Bar Title

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dashboard | MMFF Movies' });
+  res.render('index', {
+    title: 'Dashboard | MMFF Movies',
+    navBarTitle: 'Dashboard'
+  });
 });
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('movies', { title: 'Movies | MMFF Movies' });
+    res.render('movies', {
+        title: 'Movies | MMFF Movies',
+        navBarTitle: 'Movies'
+    });
 });
 
 module.exports = router;
