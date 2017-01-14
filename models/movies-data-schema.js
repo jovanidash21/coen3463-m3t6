2f7d7e07eea631eb37693b17442025bf75dd1687
jovanidash21 <jwardash21@gmail.com>
2017-01-14 23:28:09 +0800

    Update Movies Data Schema

--- models/movies-data-schema.js
+++ models/movies-data-schema.js
@@ -3,17 +3,17 @@ var moviesDataSchema = new Schema
     {
         title: {type: String, required: true},
         posterImage: {type: String, required: true},
-        starring: {type: Number, required: true, unique: true},
-        studio: {type: String, required: true},
         director: {type: String, required: true},
+        studio: {type: String, required: true},
+        starring: {type: Number, required: true, unique: true},
         genre: {type: String, required: true},
         plot: {type: String, required: true},
         imdbLink: {type: String, required: true},
         youtubeTrailerLink: {type: String, required: true},
         image: {type: String, required: true},
         grossTicketSales: {type: String, required: true},
-        createDate: {type: String, required: true},
-        updateDate: {type: String, required: true}
+        createDate: {type: Date, default: Date.now},
+        updateDate: {type: Date, default: Date.now}
     },
     {
         collection: 'moviesData'
