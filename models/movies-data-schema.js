bfc8f1c4d576fffc1af2b1d7b5efa06123e6cab4
jovanidash21 <jwardash21@gmail.com>
2017-01-15 19:19:54 +0800

    Update Movies Data Schema

--- models/movies-data-schema.js
+++ models/movies-data-schema.js
@@ -7,15 +7,46 @@ var moviesDataSchema = new Schema
     {
         title: {type: String, required: true},
         posterImage: {type: String},
-        director: {type: String},
-        studio: {type: String},
-        starring: {type: String},
+        directors: [{
+            director1: {type: String},
+            director2: {type: String},
+            director3: {type: String}
+        }],
+        studios: [{
+            studio1: {type: String},
+            studio2: {type: String},
+            studio3: {type: String},
+            studio4: {type: String},
+            studio5: {type: String}
+        }],
+        starring: [{
+            starring1: {type: String},
+            starring2: {type: String},
+            starring3: {type: String},
+            starring4: {type: String},
+            starring5: {type: String},
+            starring6: {type: String},
+            starring7: {type: String},
+            starring8: {type: String},
+            starring9: {type: String},
+            starring10: {type: String}
+        }],
         year: {type: String},
         genre: {type: String},
         plot: {type: String},
         imdbLink: {type: String},
-        youtubeTrailerLink: {type: String},
-        image: {type: String},
+        trailerLinks: [{
+            trailerLink1: {type: String},
+            trailerLink2: {type: String},
+            trailerLink3: {type: String}
+        }],
+        imageLinks: [{
+            imageLink1: {type: String},
+            imageLink2: {type: String},
+            imageLink3: {type: String},
+            imageLink4: {type: String},
+            imageLink5: {type: String}
+        }],
         grossTicketSales: {type: String}
     },
     {
