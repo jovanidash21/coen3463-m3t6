bca69affc27d106ec67160707341917166c35c6e
jovanidash21 <jwardash21@gmail.com>
2017-01-17 11:54:11 +0800

    Add Favicon

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ fs.readdirSync(__dirname + '/models').forEach(function(filename) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
