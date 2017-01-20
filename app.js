7c94516aa63cc3c5fbf2c87fdb7b716e06e4d883
jovanidash21 <jwardash21@gmail.com>
2017-01-21 00:15:37 +0800

    Remove Users Route

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ var options = {
 
 var index = require('./routes/index');
 var movies = require('./routes/movies');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -65,7 +64,6 @@ app.use(passport.session());
 
 app.use('/', index);
 app.use('/movies', movies);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
