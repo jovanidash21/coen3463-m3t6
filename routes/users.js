326b731db58ae3845b34615dba8f357164bb6050
jovanidash21 <jwardash21@gmail.com>
2017-01-26 17:35:29 +0800

    Add User Role

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var moment = require('moment');
 
 var defaultProfileImage = 'https://raw.githubusercontent.com/jovanidash21/coen3463-m3t6/master/public/images/profile_images/default.png';
 var profileImage;
+var defaultRole  = 'editor';
+var role;
 
 router.get('/', function(req, res, next) {
     if(req.user){
@@ -25,7 +27,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/add', function(req, res, next) {
-    if(req.user){
+    if(req.user.role === 'administrator'){
         res.render('users-add', {
             user: req.user,
             title: 'Add New User | MMFF Movies',
@@ -38,37 +40,45 @@ router.get('/add', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
-    if (req.body.profileImage.length == 0) {
-        profileImage = defaultProfileImage;
-    }
-    else{
-        profileImage = req.body.profileImage;
-    }
-
-    var user = {
-        username: req.body.username,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        website: req.body.website,
-        profileImage: profileImage
-    };
-    usersData.register(new usersData(user), req.body.confirmPassword, function(err) {
-        if(!err){
-            req.flash('alertMessage', 'New User Added!');
-            res.redirect('/users');
+    if(req.user.role === 'administrator'){
+        if (req.body.profileImage.length == 0) {
+            profileImage = defaultProfileImage;
         }
-        else
-        {
-            res.render('users-add', {
-                user: req.user,
-                title: 'Add New User | MMFF Movies',
-                navBarTitle: 'Add New User',
-                alertMessage: 'Sorry! User not registered because the username already exists. Please try again',
-                postError: true
-            });
+        else if (req.body.profileImage.length > 0){
+            profileImage = req.body.profileImage;
         }
-    });
+        if (req.body.role.length == 0) {
+            role = defaultRole;
+        }
+        else if (req.body.role.length > 0){
+            role = req.body.role;
+        }
+        var user = {
+            username: req.body.username,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            website: req.body.website,
+            profileImage: profileImage,
+            role: role
+        };
+        usersData.register(new usersData(user), req.body.confirmPassword, function(err) {
+            if(!err){
+                req.flash('alertMessage', 'New User Added!');
+                res.redirect('/users');
+            }
+            else
+            {
+                res.render('users-add', {
+                    user: req.user,
+                    title: 'Add New User | MMFF Movies',
+                    navBarTitle: 'Add New User',
+                    alertMessage: 'Sorry! User not registered because the username already exists. Please try again',
+                    postError: true
+                });
+            }
+        });
+    }
 });
 
 router.get('/:username', function(req, res, next) {
@@ -95,17 +105,33 @@ router.get('/:username', function(req, res, next) {
 });
 
 router.get('/:username/edit', function(req, res, next) {
-    if(req.user){
+    if (req.user){
         var username = req.params.username;
         usersData.findOne({username: username}, function (err, userData) {
             if (!err) {
-                res.render('users-edit', {
-                    user: req.user,
-                    title: 'Update User | MMFF Movies',
-                    navBarTitle: 'Update User',
-                    userData: userData,
-                    moment: moment
-                });
+                if(req.user.role === 'administrator'){
+                    res.render('users-edit', {
+                        user: req.user,
+                        title: 'Update User | MMFF Movies',
+                        navBarTitle: 'Update User',
+                        userData: userData,
+                        moment: moment
+                    });
+                }
+                else if(req.user.role === 'editor'){
+                    if (req.user.username == username){
+                        res.render('users-edit', {
+                            user: req.user,
+                            title: 'Update Profile | MMFF Movies',
+                            navBarTitle: 'Update Profile',
+                            userData: userData,
+                            moment: moment
+                        });
+                    }
+                    else{
+                        res.redirect('/');
+                    }
+                }
             }
             else {
                 res.end(err);
@@ -118,70 +144,132 @@ router.get('/:username/edit', function(req, res, next) {
 });
 
 router.post('/:username/edit', function(req, res, next) {
-    var username = req.params.username;
+    if(req.user){
+        var username = req.params.username;
 
-    if (req.body.profileImage.length == 0) {
-        profileImage = defaultProfileImage;
-    }
-    else{
-        profileImage = req.body.profileImage;
-    }
+        if (req.body.profileImage.length == 0) {
+            profileImage = defaultProfileImage;
+        }
+        else{
+            profileImage = req.body.profileImage;
+        }
 
-    usersData.findOne({username: username}, function(err, userData) {
-        if(!err){
-            userData.username = req.body.username;
-            userData.firstName = req.body.firstName;
-            userData.lastName = req.body.lastName;
-            userData.email= req.body.email;
-            userData.website = req.body.website;
-            userData.profileImage = profileImage;
-            if(req.body.password.length > 0){
-                userData.setPassword(req.body.password, function(){
-                    userData.save();
+        usersData.findOne({username: username}, function(err, userData) {
+            if(!err){
+                userData.username = req.body.username;
+                userData.firstName = req.body.firstName;
+                userData.lastName = req.body.lastName;
+                userData.email= req.body.email;
+                userData.website = req.body.website;
+                userData.profileImage = profileImage;
+                userData.role = req.body.role;
+                if(req.body.password.length > 0){
+                    userData.setPassword(req.body.password, function(){
+                        userData.save();
+                    });
+                }
+                userData.save(function(err, userData){
+                    if(!err){
+                        if(req.user.role === 'administrator'){
+                            res.render('users-edit', {
+                                user: req.user,
+                                title: 'Edit User | MMFF Movies',
+                                navBarTitle: 'Update User',
+                                alertMessage: 'User Updated',
+                                postError: false,
+                                userData: userData,
+                                moment: moment
+                            });
+                        }
+                        else if(req.user.role === 'editor') {
+                            if (req.user.username == username) {
+                                res.render('users-edit', {
+                                    user: req.user,
+                                    title: 'Update Profile | MMFF Movies',
+                                    navBarTitle: 'Update Profile',
+                                    alertMessage: 'Profile Updated',
+                                    postError: false,
+                                    userData: userData,
+                                    moment: moment
+                                });
+                            }
+                            else{
+                                res.redirect('/');
+                            }
+                        }
+                    }
+                    else{
+                        if(req.user.role === 'administrator'){
+                            res.render('users-edit', {
+                                user: req.user,
+                                title: 'Edit User | MMFF Movies',
+                                navBarTitle: 'Update User',
+                                alertMessage: 'User Not Updated! Please Try Again',
+                                postError: true,
+                                userData: userData,
+                                moment: moment
+                            });
+                        }
+                        else if(req.user.role === 'editor') {
+                            if (req.user.username == username) {
+                                res.render('users-edit', {
+                                    user: req.user,
+                                    title: 'Update Profile | MMFF Movies',
+                                    navBarTitle: 'Update Profile',
+                                    alertMessage: 'Profile Not Updated! Please Try Again',
+                                    postError: true,
+                                    userData: userData,
+                                    moment: moment
+                                });
+                            }
+                            else{
+                                res.redirect('/');
+                            }
+                        }
+                    }
                 });
             }
-            userData.save(function(err, userData){
-                if(!err){
-                    res.render('users-edit', {
-                        user: req.user,
-                        title: 'Edit User | MMFF Movies',
-                        navBarTitle: 'Update User',
-                        alertMessage: 'User Updated',
-                        postError: false,
-                        userData: userData,
-                        moment: moment
-                    });
-                }
-                else{
-                    res.render('users-edit', {
-                        user: req.user,
-                        title: 'Edit User | MMFF Movies',
-                        navBarTitle: 'Update User',
-                        alertMessage: 'User Not Updated! Please Try Again',
-                        postError: true,
-                        userData: userData,
-                        moment: moment
-                    });
-                }
-            });
-        }
-        else {
-            res.end(err);
-        }
-    });
+            else {
+                res.end(err);
+            }
+        });
+    }
+    else{
+        res.redirect('/');
+    }
 });
 
 router.post('/:username/delete', function(req, res, next) {
-    var username = req.params.username;
-    usersData.remove({username: username}, function(err){
-        if(!err){
-            req.flash('alertMessage', 'User Deleted!');
-            res.redirect('/users');
+    if(req.user){
+        var username = req.params.username;
+        if (req.user.role === 'administrator'){
+            usersData.remove({username: username}, function(err){
+                if(!err){
+                    req.flash('alertMessage', 'User Deleted!');
+                    res.redirect('/users');
+                }
+                else {
+                    res.end(err);
+                }
+            });
         }
-        else {
-            res.end(err);
+        else if(req.user.role === 'editor') {
+            if (req.user.username == username) {
+                usersData.remove({username: username}, function(err){
+                    if(!err){
+                        req.flash('alertMessage', 'Account Deleted!');
+                        res.redirect('/');
+                    }
+                    else {
+                        res.end(err);
+                    }
+                });
+            }
         }
-    });
+    }
+    else{
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
