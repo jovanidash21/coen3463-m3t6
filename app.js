209ee2352febbb2e9e8b3e98eee75c174dbb59a2
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-08-08 16:37:52 +0800

    Add Browser Sync Setup

--- app.js
+++ app.js
@@ -30,6 +30,18 @@ var users = require('./routes/users');
 
 var app = express();
 
+// browserSync setup
+if ( process.env.NODE_ENV != 'production' ) {
+  var browserSync = require('browser-sync');
+  browserSync({
+    files: ['./**/*'],
+    online: false,
+    port: 9000,
+    proxy: 'localhost:3000',
+    ui: false
+  });
+}
+
 // load all models directory files
 fs.readdirSync(__dirname + '/models').forEach(function(filename) {
   if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
