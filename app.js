5f3b2b5fcc38da68307ee7c2b3e868574f408caa
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-08-08 12:21:43 +0800

    Convert Jade To Pug

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ mongoose.connect(process.env.MONGODB_URI, options);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
